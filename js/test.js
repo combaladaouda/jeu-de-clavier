5609cb6aa14e91d16d80246d9c8553ae2fd8a8ad
combaladaouda <combaladaouda60@gmail.com>
2023-11-09 15:50:48 +0100

    first commit

---
+++ js/test.js
@@ -0,0 +1,19 @@
+let baliseImage = document.getElementById("premiereImage");
+//baliseImage.setAttribute("alt", "ceci est la nouvelle valeur de alt");
+
+baliseImage.alt = "ceci est une image test modiff";
+
+//console.log(baliseImage);
+
+baliseImage.alt = "ceci est une image de test modifiée";
+//baliseImage.classList.add("nouvelleClasse");
+baliseImage.classList.remove("photo")
+
+//console.log(baliseImage);
+
+let image = document.getElementById("premiereImage")
+
+//image.setAttribute("src", "../assert/images/2.jpg")
+
+image.src = "../assert/images/2.jpg"
+console.log(image);
